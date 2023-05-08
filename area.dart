2e8315a23691a8d72e5e917ba2747bb643b225f8
la790x <la790x@gmail.com>
2023-05-08 10:41:31 +0700

    luas script

---
+++ area.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Masukkan panjang: ");
+  double length = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukkan lebar: ");
+  double width = double.parse(stdin.readLineSync()!);
+
+  double area = length * width;
+
+  print("Luas persegi panjang adalah $area");
+}
